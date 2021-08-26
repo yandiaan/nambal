336df8d7a49a36c71b334055c5d1a34f44517267
Dian Setiawan <diansetiawan2121@gmail.com>
2021-08-26 13:21:34 +0700

    test

---
+++ aset/func.js
@@ -0,0 +1,21 @@
+let toggle = document.querySelector(".navbtn-toggle");
+toggle.addEventListener("click", function () {
+  let listLink = document.querySelector("ul");
+  if (listLink.classList.contains("none")) {
+    listLink.classList.remove("none");
+    toggle.innerHTML = "&#9747;";
+  } else {
+    listLink.classList.add("none");
+    toggle.innerHTML = "&#9776;";
+  }
+});
+
+let navbar = document.querySelector(".navbar");
+let sticky = navbar.offsetTop;
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
